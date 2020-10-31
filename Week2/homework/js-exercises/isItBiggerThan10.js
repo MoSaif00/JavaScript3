8a7f4bd44c71aff6c6cc41ab52a52a1eabd3cf27
MoSaif00 <moh97ab@hotmail.com>
2020-10-31 18:45:27 +0100

    finsiehd second exercise which check if the number bigger than 10 or not

---
+++ Week2/homework/js-exercises/isItBiggerThan10.js
@@ -0,0 +1,29 @@
+// Write a function called checkDoubleDigits that:
+// Takes 1 argument: a number
+// Returns a new Promise
+// If the number is bigger than 10, resolve with the string: "The number is bigger than 10!"
+// If the number is smaller than 10, reject with the error: "Error! The number is smaller than 10..."
+
+function checkDoubleDigits(num) {
+  // Returns a new Promise
+  return new Promise((resolve, reject) => {
+    if (num >= 10) {
+      if (num > 10) {
+        // If the number is bigger than 10, resolve with the string: "The number is bigger than 10!"
+        resolve(`The number ${num} is bigger than 10!`);
+      } else {
+        resolve('The number is Equal to 10!');
+      }
+    } else {
+      // If the number is smaller than 10, reject with the error: "Error! The number is smaller than 10..."
+      reject(new Error('Error! The number is smaller than 10...'));
+    }
+  });
+}
+
+checkDoubleDigits(15)
+  .then(console.log)
+  .catch(console.log);
+// checkDoubleDigits(5)
+//   .then(console.log)
+//   .catch(console.log);
