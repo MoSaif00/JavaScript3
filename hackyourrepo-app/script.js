9702440ed8aecb2c36c33d0b8a13f211a37d8830
MoSaif00 <moh97ab@hotmail.com>
2020-11-01 18:28:32 +0100

    disabled all html tags , and added all html codes via DOM from JavaScript

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -21,46 +21,93 @@ const placeholderRepos = [
     updated: '2020-05-27 12:00:00',
   },
 ];
+function main() {
+  const bodyContainer = document.createElement('div');
+  bodyContainer.setAttribute('class', 'container');
+  document.body.appendChild(bodyContainer);
 
-const selectRepository = document.getElementById('repositories-select');
-const displayDetails = document.getElementById('repository-details');
-// const contributorsName = document.getElementById('contributors-name');
+  const headerSection = document.createElement('section');
+  headerSection.setAttribute('class', 'header');
+  headerSection.setAttribute('id', 'header');
+  bodyContainer.appendChild(headerSection);
+  const headingHeaderText = document.createElement('h1');
+  headingHeaderText.innerText = 'HackYourFutrue Repositories';
+  headerSection.appendChild(headingHeaderText);
+  const selectTag = document.createElement('select');
+  selectTag.setAttribute('name', 'repositories');
+  selectTag.setAttribute('id', 'repositories-select');
+  headerSection.appendChild(selectTag);
 
-// displays those repositories in an alphabetically-ordered list , snippet from Stackoverflow
-placeholderRepos.sort(function sortinAlphabetically(a, b) {
-  const nameA = a.name.toUpperCase();
-  const nameB = b.name.toUpperCase();
-  if (nameA < nameB) {
-    return -1;
-  }
-  if (nameA > nameB) {
-    return 1;
-  }
-  return 0;
-});
+  const contentSection = document.createElement('section');
+  contentSection.setAttribute('class', 'page-contents');
+  bodyContainer.appendChild(contentSection);
+  const repoInfoDivTag = document.createElement('div');
+  repoInfoDivTag.setAttribute('id', 'repository-details');
+  contentSection.appendChild(repoInfoDivTag);
+  const contributorDivTag = document.createElement('div');
+  contributorDivTag.setAttribute('class', 'contributors-container');
+  contentSection.appendChild(contributorDivTag);
+  const contributeTitleTag = document.createElement('div');
+  contributeTitleTag.setAttribute('class', 'contributors-title');
+  contributorDivTag.appendChild(contributeTitleTag);
+  const contributHeadingText = document.createElement('h3');
+  contributHeadingText.innerText = 'contributors';
+  contributeTitleTag.appendChild(contributHeadingText);
+  const contributersNamesDiv = document.createElement('div');
+  contributersNamesDiv.setAttribute('id', 'contributors-name');
+  contributorDivTag.appendChild(contributersNamesDiv);
 
-// function to append the value of array into selection
-function updateOptionsToSelect(arr) {
-  arr.forEach(element => {
-    const optionsToSelect = document.createElement('option');
-    selectRepository.appendChild(optionsToSelect);
-    optionsToSelect.innerText = element.name;
-  });
-}
-updateOptionsToSelect(placeholderRepos);
+  const footerSection = document.createElement('section');
+  footerSection.setAttribute('class', 'footer');
+  document.body.appendChild(footerSection);
+  const footerText = document.createElement('h1');
+  footerText.innerText = 'HackYourFutrue Repositories';
+  footerSection.appendChild(footerText);
 
-// funtion to put the repository information at the repository details when it is selected from the select options
-function displayDetailsOfSelectedRepository() {
-  for (const element of placeholderRepos)
-    if (selectRepository.value === element.name) {
-      displayDetails.innerHTML = `
-        <h4 class="rep-title-detail">Repository : <a href="#" class="rep-name repo-detail">${element.name}</a><h4>
-        <h4 class="rep-title-detail">Description : <span class="repo-detail">${element.description}</span><h4>
-        <h4 class="rep-title-detail">Forks : <span class="repo-detail">${element.forks}</span><h4>
-        <h4 class="rep-title-detail">Updated : <span class="repo-detail">${element.updated}</span><h4>
-      `;
+  const selectRepository = document.getElementById('repositories-select');
+  const displayDetails = document.getElementById('repository-details');
+  // const contributorsName = document.getElementById('contributors-name');
+
+  // displays those repositories in an alphabetically-ordered list , snippet from Stackoverflow
+  placeholderRepos.sort(function sortinAlphabetically(a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
     }
-}
-displayDetailsOfSelectedRepository();
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
 
-selectRepository.addEventListener('change', displayDetailsOfSelectedRepository);
+  // function to append the value of array into selection
+  function updateOptionsToSelect(arr) {
+    arr.forEach(element => {
+      const optionsToSelect = document.createElement('option');
+      selectRepository.appendChild(optionsToSelect);
+      optionsToSelect.innerText = element.name;
+    });
+  }
+  updateOptionsToSelect(placeholderRepos);
+
+  // funtion to put the repository information at the repository details when it is selected from the select options
+  function displayDetailsOfSelectedRepository() {
+    for (const element of placeholderRepos)
+      if (selectRepository.value === element.name) {
+        displayDetails.innerHTML = `
+          <h4 class="rep-title-detail">Repository : <a href="#" class="rep-name repo-detail">${element.name}</a><h4>
+          <h4 class="rep-title-detail">Description : <span class="repo-detail">${element.description}</span><h4>
+          <h4 class="rep-title-detail">Forks : <span class="repo-detail">${element.forks}</span><h4>
+          <h4 class="rep-title-detail">Updated : <span class="repo-detail">${element.updated}</span><h4>
+        `;
+      }
+  }
+  displayDetailsOfSelectedRepository();
+
+  selectRepository.addEventListener(
+    'change',
+    displayDetailsOfSelectedRepository,
+  );
+}
+window.onload = main;
